ba6fcbaa22c727d6d383de4026e9aaae36ee5a7f
Martin Stensgård <martin@synq.fm>
2017-06-30 17:04:05 +0200

    synq/multipart_upload_test: use assert.{Not,}Nil

--- synq/multipart_upload_test.go
+++ synq/multipart_upload_test.go
@@ -20,84 +20,84 @@ func TestBucketOfUploadAction(t *testing.T) {
 	// real example
 	{
 		bucket, err := bucketOfUploadAction("https://synqfm.s3.amazonaws.com")
-		assert.Equal(nil, err)
+		assert.Nil(err)
 		assert.Equal("synqfm", bucket)
 	}
 
 	// another bucket
 	{
 		bucket, err := bucketOfUploadAction("https://another-bucket.s3.amazonaws.com")
-		assert.Equal(nil, err)
+		assert.Nil(err)
 		assert.Equal("another-bucket", bucket)
 	}
 
 	// yet another bucket with slash at the end
 	{
 		bucket, err := bucketOfUploadAction("https://yet-another-bucket.s3.amazonaws.com/")
-		assert.Equal(nil, err)
+		assert.Nil(err)
 		assert.Equal("yet-another-bucket", bucket)
 	}
 
 	// not https but http
 	{
 		bucket, err := bucketOfUploadAction("http://not-https.s3.amazonaws.com")
-		assert.Equal(nil, err)
+		assert.Nil(err)
 		assert.Equal("not-https", bucket)
 	}
 
 	// not the special "us-east-1" region
 	{
 		bucket, err := bucketOfUploadAction("https://a-bucket-in-another-region.s3-eu-west-1.amazonaws.com")
-		assert.Equal(nil, err)
+		assert.Nil(err)
 		assert.Equal("a-bucket-in-another-region", bucket)
 	}
 
 	// invalid region
 	{
 		bucket, err := bucketOfUploadAction("https://invalid-region.not-s3-eu-west-1.amazonaws.com")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", bucket)
 	}
 
 	// another kind of url
 	{
 		bucket, err := bucketOfUploadAction("https://uploader.synq.fm")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", bucket)
 	}
 
 	// yet another kind of url
 	{
 		bucket, err := bucketOfUploadAction("https://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115?token=not32characters")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", bucket)
 	}
 
 	// no bucket, and the bucket that is not there is not named "s3"
 	{
 		bucket, err := bucketOfUploadAction("https://s3.amazonaws.com")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", bucket)
 	}
 
 	// invalid url
 	{
 		bucket, err := bucketOfUploadAction("https://bucket.s3.amazonaws.com/%")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", bucket)
 	}
 
 	// not "amazonaws"
 	{
 		bucket, err := bucketOfUploadAction("https://bucket.s3.amazon.com/")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", bucket)
 	}
 
 	// not "com"
 	{
 		bucket, err := bucketOfUploadAction("https://bucket.s3.amazonaws.org/")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", bucket)
 	}
 
@@ -126,84 +126,84 @@ func TestRegionOfUploadAction(t *testing.T) {
 	// real example
 	{
 		region, err := regionOfUploadAction("https://synqfm.s3.amazonaws.com")
-		assert.Equal(nil, err)
+		assert.Nil(err)
 		assert.Equal("us-east-1", region)
 	}
 
 	// another region
 	{
 		region, err := regionOfUploadAction("https://a-bucket.s3-us-west-2.amazonaws.com")
-		assert.Equal(nil, err)
+		assert.Nil(err)
 		assert.Equal("us-west-2", region)
 	}
 
 	// yet another region with slash at the end
 	{
 		region, err := regionOfUploadAction("https://some-bucket.s3-ap-south-1.amazonaws.com/")
-		assert.Equal(nil, err)
+		assert.Nil(err)
 		assert.Equal("ap-south-1", region)
 	}
 
 	// not https but http
 	{
 		region, err := regionOfUploadAction("http://not-https.s3-sa-east-1.amazonaws.com")
-		assert.Equal(nil, err)
+		assert.Nil(err)
 		assert.Equal("sa-east-1", region)
 	}
 
 	// invalid region
 	{
 		region, err := regionOfUploadAction("https://invalid-region.not-s3-eu-west-1.amazonaws.com")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", region)
 	}
 
 	// another kind of url
 	{
 		region, err := regionOfUploadAction("https://player.synq.fm")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", region)
 	}
 
 	// yet another kind of url
 	{
 		region, err := regionOfUploadAction("https://player.synq.fm/embed/55d4062f99454c9fb21e5186a09c2115")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", region)
 	}
 
 	// no "s3", and the region that is not there is not named "us-east-1"
 	{
 		region, err := regionOfUploadAction("https://bucket.amazonaws.com")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", region)
 	}
 
 	// invalid url
 	{
 		region, err := regionOfUploadAction("https://bucket.s3.amazonaws.com/%")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", region)
 	}
 
 	// not "amazonaws"
 	{
 		region, err := regionOfUploadAction("https://bucket.s3.amazon.com/")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", region)
 	}
 
 	// not "com"
 	{
 		region, err := regionOfUploadAction("https://bucket.s3.amazonaws.org/")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", region)
 	}
 
 	// start with "s3", but not with "s3-"
 	{
 		region, err := regionOfUploadAction("https://invalid-region.s3eu-west-1.amazonaws.com")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", region)
 	}
 
@@ -232,35 +232,35 @@ func TestTokenOfUploaderURL(t *testing.T) {
 	// real example
 	{
 		token, err := tokenOfUploaderURL("https://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115?token=b7230fea53d525948f33abf5f4b893f5")
-		assert.Equal(nil, err)
+		assert.Nil(err)
 		assert.Equal("b7230fea53d525948f33abf5f4b893f5", token)
 	}
 
 	// non-uuid token
 	{
 		token, err := tokenOfUploaderURL("https://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115?token=not32characters")
-		assert.Equal(nil, err)
+		assert.Nil(err)
 		assert.Equal("not32characters", token)
 	}
 
 	// no url scheme
 	{
 		token, err := tokenOfUploaderURL("://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115?token=b7230fea53d525948f33abf5f4b893f5")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", token)
 	}
 
 	// no query
 	{
 		token, err := tokenOfUploaderURL("https://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", token)
 	}
 
 	// no "token" parameter
 	{
 		token, err := tokenOfUploaderURL("https://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115?notoken=notoken")
-		assert.NotEqual(nil, err)
+		assert.NotNil(err)
 		assert.Equal("", token)
 	}
 
@@ -416,6 +416,6 @@ x-amz-date:Fri, 30 Jun 2017 14:50:33 UTC
 		signature, err := UploaderSignature(uf, video_id, token, headers)
 
 		assert.Equal([]byte(expectedSignature), signature)
-		assert.Equal(nil, err)
+		assert.Nil(err)
 	}
 }
