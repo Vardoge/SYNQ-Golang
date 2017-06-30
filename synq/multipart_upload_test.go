85d7da722fa2c56a7d7f5cc9f3a739be084e3b17
Martin Stensgård <martin@synq.fm>
2017-06-30 15:21:16 +0200

    synq/multipart_upload_test: lexicographically order functions

--- synq/multipart_upload_test.go
+++ synq/multipart_upload_test.go
@@ -12,74 +12,6 @@ import (
 	"github.com/leanovate/gopter/prop"
 )
 
-func TestTokenOfUploaderURL(t *testing.T) {
-	assert := assert.New(t)
-
-	// real example
-	{
-		token, err := tokenOfUploaderURL("https://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115?token=b7230fea53d525948f33abf5f4b893f5")
-		assert.Equal(nil, err)
-		assert.Equal("b7230fea53d525948f33abf5f4b893f5", token)
-	}
-
-	// non-uuid token
-	{
-		token, err := tokenOfUploaderURL("https://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115?token=not32characters")
-		assert.Equal(nil, err)
-		assert.Equal("not32characters", token)
-	}
-
-	// no url scheme
-	{
-		token, err := tokenOfUploaderURL("://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115?token=b7230fea53d525948f33abf5f4b893f5")
-		assert.NotEqual(nil, err)
-		assert.Equal("", token)
-	}
-
-	// no query
-	{
-		token, err := tokenOfUploaderURL("https://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115")
-		assert.NotEqual(nil, err)
-		assert.Equal("", token)
-	}
-
-	// no "token" parameter
-	{
-		token, err := tokenOfUploaderURL("https://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115?notoken=notoken")
-		assert.NotEqual(nil, err)
-		assert.Equal("", token)
-	}
-
-	// any non-empty token string
-	{
-		p := gopter.NewProperties(nil)
-
-		p.Property("extract any token string", prop.ForAll(
-			func(v string) bool {
-				const base = "https://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115"
-
-				u, err := url.Parse(base)
-				if err != nil {
-					panic(err)
-				}
-
-				values := url.Values{}
-				values.Set("token", v)
-				u.RawQuery = values.Encode()
-
-				token, err := tokenOfUploaderURL(u.String())
-
-				return token == v && err == nil
-			},
-			gen.AnyString().SuchThat(
-				func(v string) bool { return v != "" },
-			),
-		))
-
-		p.TestingRun(t)
-	}
-}
-
 func TestBucketOfUploadAction(t *testing.T) {
 	assert := assert.New(t)
 
@@ -286,6 +218,74 @@ func TestRegionOfUploadAction(t *testing.T) {
 				return region == v && err == nil
 			},
 			gen.RegexMatch("^[a-z]+-[a-z]-[0-9]+$"),
+		))
+
+		p.TestingRun(t)
+	}
+}
+
+func TestTokenOfUploaderURL(t *testing.T) {
+	assert := assert.New(t)
+
+	// real example
+	{
+		token, err := tokenOfUploaderURL("https://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115?token=b7230fea53d525948f33abf5f4b893f5")
+		assert.Equal(nil, err)
+		assert.Equal("b7230fea53d525948f33abf5f4b893f5", token)
+	}
+
+	// non-uuid token
+	{
+		token, err := tokenOfUploaderURL("https://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115?token=not32characters")
+		assert.Equal(nil, err)
+		assert.Equal("not32characters", token)
+	}
+
+	// no url scheme
+	{
+		token, err := tokenOfUploaderURL("://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115?token=b7230fea53d525948f33abf5f4b893f5")
+		assert.NotEqual(nil, err)
+		assert.Equal("", token)
+	}
+
+	// no query
+	{
+		token, err := tokenOfUploaderURL("https://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115")
+		assert.NotEqual(nil, err)
+		assert.Equal("", token)
+	}
+
+	// no "token" parameter
+	{
+		token, err := tokenOfUploaderURL("https://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115?notoken=notoken")
+		assert.NotEqual(nil, err)
+		assert.Equal("", token)
+	}
+
+	// any non-empty token string
+	{
+		p := gopter.NewProperties(nil)
+
+		p.Property("extract any token string", prop.ForAll(
+			func(v string) bool {
+				const base = "https://uploader.synq.fm/uploader/55d4062f99454c9fb21e5186a09c2115"
+
+				u, err := url.Parse(base)
+				if err != nil {
+					panic(err)
+				}
+
+				values := url.Values{}
+				values.Set("token", v)
+				u.RawQuery = values.Encode()
+
+				token, err := tokenOfUploaderURL(u.String())
+
+				return token == v && err == nil
+			},
+			gen.AnyString().SuchThat(
+				func(v string) bool { return v != "" },
+			),
 		))
 
 		p.TestingRun(t)
