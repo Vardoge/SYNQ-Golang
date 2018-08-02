b6e9f1536c5274dce66bf9f2808f001fb7b68604
jessica-christie <jessica@synq.fm>
2018-08-02 22:23:48 +0530

    remove references to v1

--- helper/setup_test.go
+++ helper/setup_test.go
@@ -2,9 +2,7 @@ package helper
 
 import (
 	"testing"
-	"time"
 
-	"github.com/SYNQfm/SYNQ-Golang/synq"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -21,37 +19,9 @@ func TestGetSetup(t *testing.T) {
 	assert.Equal(SYNQ_VERSION, setup.Version)
 }
 
-func TestSetupSynq(t *testing.T) {
-	assert := assert.New(t)
-	api := SetupSynq()
-	assert.Equal(SYNQ_LEGACY_VERSION, api.Version())
-}
-
 func TestSetupSynqV2(t *testing.T) {
 	api2 := SetupSynqV2()
 	assert.Equal(t, SYNQ_VERSION, api2.Version())
-}
-
-func TestConfigure(t *testing.T) {
-	assert := require.New(t)
-	setting := ApiSetting{Url: "url", Timeout: 111, UploadTimeout: 555}
-	api := synq.NewV1("123")
-	setting.Configure(api)
-	assert.Equal(setting.Url, api.GetUrl())
-	assert.Equal(time.Duration(setting.Timeout)*time.Second, api.GetTimeout(""))
-	assert.Equal(time.Duration(setting.UploadTimeout)*time.Second, api.GetTimeout("upload"))
-}
-
-func TestSetup(t *testing.T) {
-	assert := require.New(t)
-	setting := ApiSetting{ApiKey: "123"}
-	set := ApiSet{V1: setting}
-	assert.Nil(set.ApiV1.BaseApi)
-	assert.Nil(set.ApiV2.BaseApi)
-	set.Setup()
-	assert.NotNil(set.ApiV1.BaseApi)
-	assert.Nil(set.ApiV2.BaseApi)
-	assert.Equal("123", set.ApiV1.GetKey())
 }
 
 func TestLoadFromFile(t *testing.T) {
@@ -60,10 +30,7 @@ func TestLoadFromFile(t *testing.T) {
 	assert.NotNil(err)
 	set, err := LoadFromFile(TEST_CRED_FILE)
 	assert.Nil(err)
-	assert.Equal("123", set.V1.ApiKey)
 	assert.Equal("456", set.V2.ApiKey)
-	assert.NotNil(set.ApiV1.BaseApi)
 	assert.NotNil(set.ApiV2.BaseApi)
-	assert.Equal("123", set.ApiV1.GetKey())
 	assert.Equal("456", set.ApiV2.GetKey())
 }
